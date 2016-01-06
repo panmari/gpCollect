0f28e8dd73e07f664ab4002e96ac7a8f7ff367b4
panmari <smmuzi@gmail.com>
2016-01-06 11:17:21 +0100

    Fixed I18n for show_runner_chart.

--- app/charts/show_runner_chart.rb
+++ app/charts/show_runner_chart.rb
@@ -11,16 +11,16 @@ class ShowRunnerChart < RuntimeChart
     data = make_runs_data(runner) do |run|
       run.interim_times[1]
     end
-    self.series(name: t('show_runner_chart.at_10_km'), data: data)
+    self.series(name: I18n.t('show_runner_chart.at_10_km'), data: data)
 
     data = make_runs_data(runner) do |run|
       run.interim_times[0]
     end
-    self.series(name: t('show_runner_chart.at_5_km'), data: data)
+    self.series(name: I18n.t('show_runner_chart.at_5_km'), data: data)
 
     data = make_runs_data(runner) do |run|
       run.run_day_category_aggregate.mean_duration
     end
-    self.series(name: t('show_runner_chart.mean_category'), data: data, type: 'line')
+    self.series(name: I18n.t('show_runner_chart.mean_category'), data: data, type: 'line')
   end
 end
