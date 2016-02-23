eb2269edc9c4df3f7ab1127b5e9beb9dda7e35dc
panmari <smmuzi@gmail.com>
2016-02-23 17:36:50 +0100

    Caching CPU intensive gender_only_categories
    Mainly because the production server is a potato.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,7 +8,10 @@ class CategoriesController < ApplicationController
     @chart = CompareCategoriesChart.new(@categories, 'mean')
     @min_duration_chart = CompareCategoriesChart.new(@categories, 'min')
     @participant_chart = ParticipantsChart.new(@categories)
-    gender_only_categories = aggregate_to(@categories, :sex, [:M, :W])
+    cache_id = @categories.map(&:run_day_category_aggregates).flatten.map(&:id).join(',')
+    gender_only_categories = Rails.cache.fetch(cache_id) do
+      aggregate_to(@categories, :sex, [:M, :W])
+    end
     @participant_gender_chart = ParticipantsChart.new(gender_only_categories)
     render 'show'
   end
