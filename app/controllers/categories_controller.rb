88e3acd8b00bede6b76ca90d3a0dc12af08a07f4
panmari <smmuzi@gmail.com>
2016-02-23 17:35:50 +0100

    Proper handling of unexpected case via warning.
    This should not occur, but is not really worth an exception (too much of
    a hassle). Just giving out a warning here.
    
    Future me will propably not approve of this commit ;)

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -31,7 +31,12 @@ class CategoriesController < ApplicationController
     RunDay.all.each do |run_day|
       run_day_agg = values.each_with_object({}) { |v, h| h[v] = OpenStruct.new(runs_count: 0, run_day: run_day) }
       categories.each do |c|
-        run_day_agg[c.send(attribute).to_sym].runs_count += c.run_day_category_aggregates.find { |a| a.run_day == run_day }&.runs_count || 0
+        corresponding_run_day = c.run_day_category_aggregates.find { |a| a.run_day == run_day }
+        if corresponding_run_day
+          run_day_agg[c.send(attribute).to_sym].runs_count += corresponding_run_day.runs_count
+        else
+          Rails.logger.warn("Could not find corresponding entry for #{run_day.inspect} in #{c.run_day_category_aggregates.inspect}")
+        end
       end
       values.each { |v| values_hash[v].run_day_category_aggregates << run_day_agg[v] }
     end
