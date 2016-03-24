5fd6fa453749191a3ba34b167ff1c64efc0944eb
panmari <smmuzi@gmail.com>
2016-03-24 22:14:05 +0100

    Added interim times to table on runner view.

--- app/decorators/run_decorator.rb
+++ app/decorators/run_decorator.rb
@@ -5,4 +5,15 @@ class RunDecorator < Draper::Decorator
   def duration_formatted
     h.format_duration(object.duration)
   end
+
+
+  def interim_time_formatted(idx)
+    time = object.interim_times[idx]
+    if time
+      h.format_duration(time)
+    else
+      '-'
+    end
+  end
+
 end
