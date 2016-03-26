8eae2f262ab8972ef5d080c51750dff124b06bcf
panmari <smmuzi@gmail.com>
2016-03-26 09:55:03 +0100

    Prettier display of category links.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,8 @@
 module CategoriesHelper
+
+  def category_button_group(categories)
+    content_tag :div, class: 'btn-group btn-group-justified', role: 'group' do
+      categories.map {|c| link_to(c.name, c, class: 'btn btn-default')}.join('').html_safe
+    end
+  end
 end
