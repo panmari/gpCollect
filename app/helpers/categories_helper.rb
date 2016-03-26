681cd3582e0247ac23dcd7ce4971a01f54930ee5
panmari <smmuzi@gmail.com>
2016-03-26 13:21:33 +0100

    Added some styling for category buttons.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,8 +1,15 @@
 module CategoriesHelper
 
-  def category_button_group(categories)
-    content_tag :div, class: 'btn-group btn-group-justified', role: 'group' do
-      categories.map {|c| link_to(c.name, c, class: 'btn btn-default')}.join('').html_safe
+  def category_button_group(categories, button_class='btn-default')
+    content_tag :div, class: 'row' do
+      split_index = categories.size / 2
+      [categories[0...split_index], categories[split_index..-1]].map do |category_slice|
+        content_tag :div, class: 'col-sm-6' do
+          content_tag :div, class: 'btn-group btn-group-justified', role: 'group' do
+            category_slice.map { |c| link_to(c.name, c, class: 'btn ' + button_class) }.join.html_safe
+          end
+        end
+      end.join.html_safe
     end
   end
 end
