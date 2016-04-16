35ece24da26d02722432b3bfd04fd4dafa95201f
panmari <smmuzi@gmail.com>
2016-04-16 21:34:29 +0200

    Removed unused code, added more associations.

--- app/models/run_day.rb
+++ app/models/run_day.rb
@@ -2,6 +2,7 @@ class RunDay < ActiveRecord::Base
   belongs_to :organizer
   belongs_to :route
   has_many :runs
+  has_many :runners, through: :runs
   has_many :run_day_category_aggregates
   has_many :categories, through: :run_day_category_aggregates
 
