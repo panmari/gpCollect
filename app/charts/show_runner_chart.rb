8fbd2430cf2f5486a6d8b973b9d64bc7d527751e
panmari <smmuzi@gmail.com>
2016-05-15 14:56:15 +0200

    Updated color theme for show page.

--- app/charts/show_runner_chart.rb
+++ app/charts/show_runner_chart.rb
@@ -6,27 +6,27 @@ class ShowRunnerChart < RuntimeChart
     data = make_runs_data(runner) do |run|
       run.duration
     end
-    self.series(name: I18n.t('show_runner_chart.goal'), data: data)
+    self.series(name: I18n.t('show_runner_chart.goal'), data: data, color: '#5596DE')
 
     data = make_runs_data(runner) do |run|
       run.interim_times[2]
     end
-    self.series(name: I18n.t('show_runner_chart.at_12_5_km'), data: data)
+    self.series(name: I18n.t('show_runner_chart.at_12_5_km'), data: data, color: '#3884D9')
 
     data = make_runs_data(runner) do |run|
       run.interim_times[1]
     end
-    self.series(name: I18n.t('show_runner_chart.at_10_km'), data: data)
+    self.series(name: I18n.t('show_runner_chart.at_10_km'), data: data, color: '#0968D2')
 
     data = make_runs_data(runner) do |run|
       run.interim_times[0]
     end
-    self.series(name: I18n.t('show_runner_chart.at_5_km'), data: data)
+    self.series(name: I18n.t('show_runner_chart.at_5_km'), data: data, color: '#074F9F')
 
     data = make_runs_data(runner) do |run|
       run.run_day_category_aggregate.mean_duration
     end
-    self.series(name: I18n.t('show_runner_chart.mean_category'), data: data, type: 'line')
+    self.series(name: I18n.t('show_runner_chart.mean_category'), data: data, type: 'line', color: '#222222')
     # TODO: Make custom color theme for better readability.
   end
 end
