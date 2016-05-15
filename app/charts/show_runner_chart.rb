f85351c847f79b8e188d672cd50babacb8df9b29
panmari <smmuzi@gmail.com>
2016-05-15 13:35:32 +0200

    Added support for 3rd interim time on view side.

--- app/charts/show_runner_chart.rb
+++ app/charts/show_runner_chart.rb
@@ -9,6 +9,11 @@ class ShowRunnerChart < RuntimeChart
     self.series(name: I18n.t('show_runner_chart.goal'), data: data)
 
     data = make_runs_data(runner) do |run|
+      run.interim_times[2]
+    end
+    self.series(name: I18n.t('show_runner_chart.at_12_5_km'), data: data)
+
+    data = make_runs_data(runner) do |run|
       run.interim_times[1]
     end
     self.series(name: I18n.t('show_runner_chart.at_10_km'), data: data)
@@ -22,5 +27,6 @@ class ShowRunnerChart < RuntimeChart
       run.run_day_category_aggregate.mean_duration
     end
     self.series(name: I18n.t('show_runner_chart.mean_category'), data: data, type: 'line')
+    # TODO: Make custom color theme for better readability.
   end
 end
