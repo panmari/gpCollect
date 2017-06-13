defa1b2dd7058c6fc094089ff850d643a1f8392a
panmari <smmuzi@gmail.com>
2017-06-13 18:40:57 +0200

    Changing 'modern' category scope to join.
    Using 'includes' has weird side effects with prefetching not working as
    intended: Even though multiple run_day_category_aggregates should be
    present, only one is there.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,16 +1,19 @@
 class Category < ActiveRecord::Base
   has_many :runs
   has_many :run_day_category_aggregates
+  has_many :run_days, through: :run_day_category_aggregates
 
   scope :ordered, -> { order(:age_max, :age_min, :sex)}
 
   MODERN_RUNS_YEAR = 2016
   # All categories that occured since the refresh. Older runs have slightly
-  # different categories.
+  # different categories. A normal 'includes' does not work, as prefetching
+  # of run_day_category_aggregates does not work correctly then.
   scope :modern, -> { joins(run_day_category_aggregates: :run_day)
-    .where('extract(year from run_days.date) = ?', MODERN_RUNS_YEAR)
     .where('runs_count > ?', 0).references(:run_day_category_aggregates)
+    .where('extract(year from run_days.date) = ?', MODERN_RUNS_YEAR).references(:run_days)
   }
+
 
   def name
     sex + if age_max
