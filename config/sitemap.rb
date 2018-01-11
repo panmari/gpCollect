53594176779bf672293d845d8c49126c1bf122d9
panmari <smmuzi@gmail.com>
2018-01-11 18:46:16 +0100

    Fixed issues with sitemap generating invalid URLs.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,11 +4,12 @@ SitemapGenerator::Sitemap.default_host = "http://gpcollect.duckdns.org"
 SitemapGenerator::Sitemap.create do
 
   Category.find_each.each do |category|
-    add(category_url(category, locale: I18n.default_locale),
+    add(category_path(category, locale: I18n.default_locale),
         changefreq: 'yearly',
         lastmod: category.updated_at,
-        alternates: [{
-                          lang: 'en', href: category_url(category, locale: 'en')}
-        ])
+        alternates: I18n.available_locales.reject {|l|
+                        l == I18n.default_locale}.map { |l|
+                        { lang: l, href: category_url(category, locale: l)}
+        })
   end
 end
