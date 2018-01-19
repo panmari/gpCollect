d9ac6022dd639c5b049a3a6deac92d7cfbb2afc0
panmari <smmuzi@gmail.com>
2018-01-19 22:59:57 +0100

    More changes to geocoder.
    1. Moving known prefixes to helper file.
    2. Removing fail_cache, setting 'false' in cache instead.
    3. Running rubocop on all files.

--- geocoder_run_year.rb
+++ geocoder_run_year.rb
@@ -1,11 +1,14 @@
 require 'ruby-progressbar'
 load 'geocoder.rb'
 
-geocoder_cache_file = "geocoder_cache.json"
-geocoder = Geocoder.new(geocoder_cache_file, 'ags.list')
+year = 2017
+geocoder_cache_file = 'geocoder_cache.json'
+geocoder = Geocoder.new(geocoder_cache_file,
+                        'ags.list',
+                        'ignored_prefixes.list')
 lat_long_weights = Hash.new(0)
 begin
-  RunDay.last.runners.each do |runner|
+  RunDay.find_by_year!(year).runners.each do |runner|
     lat_long, place = geocoder.find_lat_long_for(runner)
     if lat_long.blank?
       puts "Fail: #{place}"
@@ -19,8 +22,7 @@ rescue Exception => e
   puts e.backtrace.join("\n")
 ensure
   geocoder.dump_cache(geocoder_cache_file)
-  File.open("lat_long_weights.json", "w") do |f|
+  File.open("lat_long_weights_#{year}.json", 'w') do |f|
     f.write(lat_long_weights.to_json)
   end
 end
-
