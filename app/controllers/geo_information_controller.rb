01b529ee4d165dd3bb2ecfaebe68c3d028b85d1d
panmari <smmuzi@gmail.com>
2018-01-27 20:13:28 +0100

    Added logic for toggling trough data of multiple years.

--- app/controllers/geo_information_controller.rb
+++ app/controllers/geo_information_controller.rb
@@ -1,6 +1,6 @@
 class GeoInformationController < ApplicationController
   def index
-    @lat_lng_weights = (2016..2017).map do |year|
+    @lat_lng_weights = (2010..2017).map do |year|
       data_as_array = JSON.parse(File.open("lat_long_weights_#{year}.json").read,
                                  symbolize_names: true)
                           .map { |lat_lng, weight| [lat_lng[:lat], lat_lng[:lng], weight] }
