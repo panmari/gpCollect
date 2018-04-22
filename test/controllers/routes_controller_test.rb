51b44f466ad4b2111be73fce2cbedce450923c5a
panmari <smmuzi@gmail.com>
2018-04-22 23:01:07 +0200

    Basic test for routes.

---
+++ test/controllers/routes_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class RoutesControllerTest < ActionController::TestCase
+  include Devise::Test::ControllerHelpers
+  setup do
+    @route = create(:route)
+  end
+
+  test 'should show route' do
+    get :show, params: { id: @route.id }
+    assert_response :success
+  end
+end
