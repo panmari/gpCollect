785df86d6c91eab535de243d715b342bcc837d77
panmari <smmuzi@gmail.com>
2018-04-26 22:42:55 +0200

    Extending sitemap to include routes.

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,15 +1,16 @@
 # Set the host name for URL creation
-SitemapGenerator::Sitemap.default_host = "http://gpcollect.duckdns.org"
+SitemapGenerator::Sitemap.default_host = 'http://gpcollect.duckdns.org'
 
+OTHER_LOCALES = I18n.available_locales.reject { |l| l == I18n.default_locale }
 SitemapGenerator::Sitemap.create do
-
   Category.find_each.each do |category|
     add(category_path(category, locale: I18n.default_locale),
         changefreq: 'yearly',
         lastmod: category.updated_at,
-        alternates: I18n.available_locales.reject {|l|
-                        l == I18n.default_locale}.map { |l|
-                        { lang: l, href: category_url(category, locale: l)}
-        })
+        alternates: OTHER_LOCALES.map { |l| { lang: l, href: category_url(category, locale: l) } })
+  end
+  Route.find_each.each do |route|
+    add(route_path(route, locale: I18n.default_locale),
+        alternates: OTHER_LOCALES.map { |l| { lang: l, href: route_url(route, locale: l) } })
   end
 end
