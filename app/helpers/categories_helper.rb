8e4fb81d8e69710ef653e57fbc6a029785eb87fe
panmari <smmuzi@gmail.com>
2018-05-12 11:59:30 +0200

    Fixed categories and readded categories path to navbar.

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,11 +1,12 @@
 module CategoriesHelper
-  def category_button_group(categories, active_category, button_class = 'btn-default')
+  def category_button_group(categories, active_category,
+                            button_class = 'btn-default')
     content_tag :div, class: 'row' do
       split_index = categories.size / 2
       [categories[0...split_index], categories[split_index..-1]].map do |category_slice|
         content_tag :div, class: 'col-sm-6' do
-          content_tag :div, class: 'btn-group btn-group-justified', role: 'group' do
-            basic_css_class = 'btn ' + button_class
+          content_tag :div, class: 'btn-group d-flex', role: 'group' do
+            basic_css_class = 'btn w-100 ' + button_class
             category_slice.map do |c|
               css_class = if c == active_category
                             basic_css_class + ' active'
