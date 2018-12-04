c975e20e8d145d197dd12e708b86861c9f8c1f22
panmari <smmuzi@gmail.com>
2018-12-04 09:11:16 +0100

    Removing unused method.
    Could not find any references to this method anymore.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CategoriesController < ApplicationController
   before_action :set_category, only: [:show]
   before_action :set_categories
@@ -31,26 +33,6 @@ class CategoriesController < ApplicationController
   end
 
   private
-
-  # Creates new 'run_day_category_aggregates' by aggregating existing ones by some grouping mechanism `attribute`
-  # that can take on the values defined by `values`.
-  def aggregate_to(categories, attribute, values)
-    values.map! &:to_sym
-    values_hash = values.each_with_object({}) { |v, h| h[v] = OpenStruct.new(run_day_category_aggregates: [], name: v) }
-    RunDay.all.each do |run_day|
-      run_day_agg = values.each_with_object({}) { |v, h| h[v] = OpenStruct.new(runs_count: 0, run_day: run_day) }
-      categories.each do |c|
-        corresponding_run_day = c.run_day_category_aggregates.find { |a| a.run_day == run_day }
-        if corresponding_run_day
-          run_day_agg[c.send(attribute).to_sym].runs_count += corresponding_run_day.runs_count
-        else
-          Rails.logger.warn("Could not find corresponding entry for #{run_day.inspect} in #{c.run_day_category_aggregates.inspect}")
-        end
-      end
-      values.each { |v| values_hash[v].run_day_category_aggregates << run_day_agg[v] }
-    end
-    values_hash.values
-  end
 
   def set_categories
     @categories = Category.modern.ordered
