dbbf15793e3a667fee08cbbcb27bc1dec9b9807d
panmari <smmuzi@gmail.com>
2018-12-26 09:01:24 +0100

    Adding support for destroying geocode_results.
    By first removing association from all objects.

--- app/models/geocode_result.rb
+++ app/models/geocode_result.rb
@@ -1,3 +1,10 @@
 class GeocodeResult < ActiveRecord::Base
   has_many :runners
+  before_destroy :remove_from_runners
+
+  private
+
+  def remove_from_runners
+    runners.update_all(geocode_result_id: nil)
+  end
 end
