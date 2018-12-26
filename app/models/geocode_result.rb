ec8c930999d03ddf9c667f8b2fcc2491bb99eb48
panmari <smmuzi@gmail.com>
2018-12-26 14:51:02 +0100

    Adding more basic functionality to GeocodeResult.
    For easier interacting with 'results' json type.

--- app/models/geocode_result.rb
+++ app/models/geocode_result.rb
@@ -1,6 +1,21 @@
+# frozen_string_literal: true
+
 class GeocodeResult < ActiveRecord::Base
   has_many :runners
   before_destroy :remove_from_runners
+  scope :failed, -> { where("response ->> 'status' != 'OK'") }
+  scope :ambiguous, -> { where("JSON_ARRAY_LENGTH(response -> 'results') > ?", 1) }
+
+  def canton
+    return nil if response['results'].empty?
+
+    canton_result = response['results'].first['address_components'].find do |c|
+      c['types'].include?('administrative_area_level_1')
+    end
+    return nil if canton_result.nil?
+
+    canton_result['long_name']
+  end
 
   private
 
