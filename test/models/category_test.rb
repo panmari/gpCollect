f472b345cb15482431020c77c51edaea75d2eb71
panmari <smmuzi@gmail.com>
2018-12-30 21:12:07 +0100

    Increasing test coverage for 'U' style categories.

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -1,11 +1,18 @@
 require 'test_helper'
 
 class CategoryTest < ActiveSupport::TestCase
-  setup do
+  test "find categoy by name" do
     @category = create(:category_M30)
+    assert_equal Category.find_by_name('M30'), @category
   end
 
-  test "find categoy by name" do
-    assert_equal Category.find_by_name('M30'), @category
+  test "find category by name with age max" do
+    @category = create(:category, sex: 'W', age_max: 18)
+    assert_equal Category.find_by_name('WU18'), @category
+  end
+
+  test "should return correct name for category with age max" do
+    @category = create(:category, sex: 'W', age_max: 18)
+    assert_equal 'WU18', @category.name
   end
 end
