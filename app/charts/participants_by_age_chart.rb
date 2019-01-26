ab87c8c6db66fe2b98f5fe36cc7a7c8472cd99b8
panmari <smmuzi@gmail.com>
2019-01-26 17:46:46 +0100

    Some small fixes to participants charts.

--- app/charts/participants_by_age_chart.rb
+++ app/charts/participants_by_age_chart.rb
@@ -6,9 +6,12 @@ class ParticipantsByAgeChart < ParticipantsChart
     legend(layout: 'horizontal')
 
     # Only min or max is set. LEAST is used to reject the 'NULL' value.
+    # 20 exists both as M20 and MU20 in some years. By always subtracting 1 from
+    # age_max, we guarantee that all MU categories (which are all under 20) land
+    # in the first bucket.
     agg = RunDay.joins(run_day_category_aggregates: :category)
-                .group([:date, "(LEAST(age_min, age_max) / #{AGE_BUCKET_SIZE}) * #{AGE_BUCKET_SIZE}"])
-                .order([:date, "least_age_min_age_max_#{AGE_BUCKET_SIZE}_all_#{AGE_BUCKET_SIZE}"])
+                .group([:date, "(LEAST(age_min, age_max - 1) / #{AGE_BUCKET_SIZE}) * #{AGE_BUCKET_SIZE}"])
+                .order([:date, "least_age_min_age_max_1_#{AGE_BUCKET_SIZE}_all_#{AGE_BUCKET_SIZE}"])
                 .sum(:runs_count)
     keyed_by_age_bucket = agg.each_with_object({}) do |row, h|
       key, count = *row
@@ -18,7 +21,7 @@ class ParticipantsByAgeChart < ParticipantsChart
     keyed_by_age_bucket.each do |age, data|
       series(data: data,
              name: I18n.t('participants_chart.age_bucket',
-                          age_bucket: "#{age}-#{age + AGE_BUCKET_SIZE}"))
+                          age_bucket: "#{age}-#{age + AGE_BUCKET_SIZE - 1}"))
     end
   end
 end
